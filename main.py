fffef281400b9dffa06b33318d8290d49ff9e165
Oscarhm408 <101809231+Oscarhm408@users.noreply.github.com>
2023-02-23 19:41:58 -0800

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,7 @@
 # This example requires the 'message_content' privileged intents
 
 import os
+import random
 import discord
 from discord.ext import commands
 
@@ -9,7 +10,7 @@ intents = discord.Intents.default()
 intents.message_content = True
 bot = commands.Bot(command_prefix='!', intents=intents)
 
-
+list = ["1", "2"]
 @bot.event
 async def on_ready():
     print(f"Logged in as {bot.user}")
@@ -20,7 +21,7 @@ async def ping(ctx):
 
 @bot.command()
 async def hello(ctx):
-    await ctx.send("Choo choo! 🚅")
+    await ctx.send(random.choice(list)
 
 
 bot.run(os.environ["DISCORD_TOKEN"])
