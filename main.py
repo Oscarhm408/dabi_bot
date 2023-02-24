98eeb0d11368ad2017928595d2acc5b77b5e8434
Oscarhm408 <101809231+Oscarhm408@users.noreply.github.com>
2023-02-23 20:38:23 -0800

    Update main.py

--- main.py
+++ main.py
@@ -26,11 +26,11 @@ async def hello(ctx):
     
 @bot.command()
 async def praise(ctx):
-    await ctx.send(random.choice(os.environ["praise_list"]))
+    await ctx.send(random.choice(json.loads(os.environ["praise_list"])))
     
 @bot.command()
 async def dabi(ctx):
-    await ctx.send(random.choice(os.environ["dabi_quotes"]))
+    await ctx.send(random.choice(json.loads(os.environ["dabi_quotes"])))
 
                    
 bot.run(os.environ["DISCORD_TOKEN"])
